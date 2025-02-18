f8af6515565d8a02a777efd08eeed5c88ec45bda
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2025-02-18 12:16:04 +0100

    Added custom toggle button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 "use client"
 import useLocalStorage from "@/hooks/useLocalStorage";
 import Card from "./components/Card";
+import useToggle from "@/hooks/useToggle";
 
 export default function Home() {
   const [name, setName] = useLocalStorage("name", "");
+  const [value, toggleValue] = useToggle(false);
   return (
     <div>
       <Card name={"Test"} />
+      <div>
+        <button onClick={toggleValue}>Toggle</button>
+        <button onClick={() => toggleValue(true)}>Make True</button>
+        <button onClick={() => toggleValue(false)}>Make False</button>
+      </div>
+      <div>{value.toString()}</div>
       <input type="text" value={name} onChange={e => setName(e.target.value)} />
     </div>
   );
